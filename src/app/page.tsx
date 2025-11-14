a45c6a8c366c7d95592ed7fb12ea0ae40f187fd5
arfandotid <arfandotid@gmail.com>
2025-11-14 10:25:33 +0700

    setup darkmode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { DarkModeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Input />
       <Button>Hello World</Button>
+      <DarkModeToggle />
     </div>
   );
 }
