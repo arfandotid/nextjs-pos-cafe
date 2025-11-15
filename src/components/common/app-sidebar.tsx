b204f81546ff8a307ab105bf68577dde0f4894f2
arfandotid <arfandotid@gmail.com>
2025-11-15 12:14:30 +0700

    logout

--- src/components/common/app-sidebar.tsx
+++ src/components/common/app-sidebar.tsx
@@ -29,6 +29,7 @@ import {
 } from "@/constants/sidebar-constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { signOut } from "@/actions/auth-action";
 
 export default function AppSidebar({}) {
   const { isMobile } = useSidebar();
@@ -124,7 +125,7 @@ export default function AppSidebar({}) {
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
-                  <DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => signOut()}>
                     <LogOut />
                     Logout
                   </DropdownMenuItem>
