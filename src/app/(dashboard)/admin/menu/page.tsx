e49aea1df0d543cbcfea39e5e49100f67675c790
arfandotid <arfandotid@gmail.com>
2025-11-17 20:58:34 +0700

    list menu table

---
+++ src/app/(dashboard)/admin/menu/page.tsx
@@ -0,0 +1,9 @@
+import MenuManagement from "./_components/menu";
+
+export const metadata = {
+  title: "Menu Management",
+};
+
+export default function MenuManagementPage() {
+  return <MenuManagement />;
+}
