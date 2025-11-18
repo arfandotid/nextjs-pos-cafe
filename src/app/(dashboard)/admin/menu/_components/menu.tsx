dffe8643fbec92f26d924f545f23f56aa10537f5
arfandotid <arfandotid@gmail.com>
2025-11-18 10:23:01 +0700

    delete data menu

--- src/app/(dashboard)/admin/menu/_components/menu.tsx
+++ src/app/(dashboard)/admin/menu/_components/menu.tsx
@@ -17,6 +17,7 @@ import { cn, convertIDR } from "@/lib/utils";
 import { HEADER_TABLE_MENU } from "@/constants/menu-constant";
 import DialogCreateMenu from "./dialog-create-menu";
 import DialogUpdateMenu from "./dialog-update-menu";
+import DialogDeleteMenu from "./dialog-delete-menu";
 
 export default function UserManagement() {
   const supabase = createClient();
@@ -164,6 +165,12 @@ export default function UserManagement() {
       />
       <DialogUpdateMenu
         open={selectedAction !== null && selectedAction.type === "update"}
+        refetch={refetch}
+        currentData={selectedAction?.data}
+        handleChangeAction={handleChangeAction}
+      />
+      <DialogDeleteMenu
+        open={selectedAction !== null && selectedAction.type === "delete"}
         refetch={refetch}
         currentData={selectedAction?.data}
         handleChangeAction={handleChangeAction}
