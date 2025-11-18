78397c20521c2ef0194a033fadb805cffc767cbc
arfandotid <arfandotid@gmail.com>
2025-11-18 10:18:04 +0700

    update table menu

--- src/app/(dashboard)/admin/menu/_components/menu.tsx
+++ src/app/(dashboard)/admin/menu/_components/menu.tsx
@@ -16,6 +16,7 @@ import Image from "next/image";
 import { cn, convertIDR } from "@/lib/utils";
 import { HEADER_TABLE_MENU } from "@/constants/menu-constant";
 import DialogCreateMenu from "./dialog-create-menu";
+import DialogUpdateMenu from "./dialog-update-menu";
 
 export default function UserManagement() {
   const supabase = createClient();
@@ -160,6 +161,12 @@ export default function UserManagement() {
         currentLimit={currentLimit}
         onChangePage={handleChangePage}
         onChangeLimit={handleChangeLimit}
+      />
+      <DialogUpdateMenu
+        open={selectedAction !== null && selectedAction.type === "update"}
+        refetch={refetch}
+        currentData={selectedAction?.data}
+        handleChangeAction={handleChangeAction}
       />
     </div>
   );
