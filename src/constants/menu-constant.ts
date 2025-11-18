0d1adb7907685ffefcdd0424bce32b2202120e71
arfandotid <arfandotid@gmail.com>
2025-11-18 09:55:19 +0700

    create menu

--- src/constants/menu-constant.ts
+++ src/constants/menu-constant.ts
@@ -6,3 +6,54 @@ export const HEADER_TABLE_MENU = [
   "Availability",
   "Action",
 ];
+
+export const CATEGORY_LIST = [
+  {
+    value: "beverage",
+    label: "Beverage",
+  },
+  {
+    value: "mains",
+    label: "Mains",
+  },
+  {
+    value: "desserts",
+    label: "Desserts",
+  },
+];
+
+export const MENU_AVAILABILITY_LIST = [
+  {
+    value: "true",
+    label: "Available",
+  },
+  {
+    value: "false",
+    label: "Not Available",
+  },
+];
+
+export const INITIAL_MENU = {
+  name: "",
+  description: "",
+  price: "",
+  discount: "",
+  category: "",
+  image_url: "",
+  is_available: "",
+};
+
+export const INITIAL_STATE_MENU = {
+  status: "idle",
+  errors: {
+    id: [],
+    name: [],
+    description: [],
+    price: [],
+    discount: [],
+    category: [],
+    image_url: [],
+    is_available: [],
+    _form: [],
+  },
+};
