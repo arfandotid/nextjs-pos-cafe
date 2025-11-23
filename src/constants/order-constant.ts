daea23267c225338dba80b7610edb5972e229f86
arfandotid <arfandotid@gmail.com>
2025-11-23 12:06:10 +0700

    generate payment midtrans

--- src/constants/order-constant.ts
+++ src/constants/order-constant.ts
@@ -1,3 +1,4 @@
+import { INITIAL_STATE_ACTION } from "./general-constant";
 export const HEADER_TABLE_ORDER = [
   "No",
   "Order ID",
@@ -64,3 +65,10 @@ export const FILTER_MENU = [
     label: "Beverages",
   },
 ];
+
+export const INITIAL_STATE_GENERATE_PAYMENT = {
+  ...INITIAL_STATE_ACTION,
+  data: {
+    payment_token: "",
+  },
+};
